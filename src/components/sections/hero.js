dee1e5cc5c204a90cf77fc952ff3d02da7a7a676
Vedang Wartikar <vedangwartikar17@gmail.com>
2021-09-21 00:51:54 +0530

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -58,17 +58,16 @@ const Hero = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const one = <h1>Hello World! My name is</h1>;
+  const two = <h2 className="big-heading">Vedang Warikar.</h2>;
+  const three = <h3 className="big-heading">I love to automate stuff.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a Software Engineer from India. Currently, I’m building streamline products
+        at{' '} to make open-source softwares more accessible to the community. My work revolves around Linux and containerization technologies.
+        <a href="https://persistent.com/" target="_blank" rel="noreferrer">
+          Persistent
         </a>
         .
       </p>
@@ -77,10 +76,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://linkedin.vedang.dev"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Connect with me
     </a>
   );
 
