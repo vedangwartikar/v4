f93c39085dc4adda3286ddc21106a075d7ae7a75
Vedang Wartikar <vedangwartikar17@gmail.com>
2021-09-20 23:44:48 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Vedang Wartikar',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Vedang Wartikar is a software engineer who likes to engineer software',
+    siteUrl: 'https://fancyfolio.vercel.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@vedangwartikar',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'VedangWartikar',
+        short_name: 'VedangWartikar',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
